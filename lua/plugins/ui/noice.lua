e44c14562a7ec424a5ef35863f95711bd2345627
vuongprogk <vuong753333@gmail.com>
2024-10-15 03:32:52 +0700

    fix: remove lsp no information available notifications

--- lua/plugins/ui/noice.lua
+++ lua/plugins/ui/noice.lua
@@ -44,6 +44,13 @@ return {
         },
         view = "mini",
       },
+      {
+        filter = {
+          event = "notify",
+          find = "No information available",
+        },
+        opts = { skip = true },
+      },
     },
     presets = {
       bottom_search = true,
