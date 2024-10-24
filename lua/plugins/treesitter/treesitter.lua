d7761fc0fcb3e120479072f4319427ea0342c66b
vuongprogk <vuong753333@gmail.com>
2024-10-24 19:31:06 +0700

    fix: disable treesitter highlight on large file

---
+++ lua/plugins/treesitter/treesitter.lua
@@ -0,0 +1,21 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  opts = function(_, opts)
+    opts.highlight = {
+      enable = true,
+      disable = function(lang, buf)
+        local max_filesize = 100 * 1024 -- 100 KB
+        local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+        if ok and stats and stats.size > max_filesize then
+          return true
+        end
+      end,
+    }
+  end,
+  config = function(_, opts)
+    if type(opts.ensure_installed) == "table" then
+      opts.ensure_installed = LazyVim.dedup(opts.ensure_installed)
+    end
+    require("nvim-treesitter.configs").setup(opts)
+  end,
+}
