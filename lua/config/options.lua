35cc09bbf09faf5b613097f335600dfd1bcd75d9
vuongprogk <vuong753333@gmail.com>
2024-11-21 02:32:00 +0700

    feat: update options in lazyvim

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,13 +1,14 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-_G.lazyvim_docs = true
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 vim.g.loaded_perl_provider = 0
 vim.g.loaded_ruby_provider = 0
 vim.g.loaded_python3_provider = 0
 vim.g.loaded_node_provider = 0
+vim.g.lazyvim_blink_main = true
+vim.g.lazyvim_php_lsp = "intelephense"
 local opt = vim.opt
 opt.wildmode = "full"
 opt.ttyfast = true
