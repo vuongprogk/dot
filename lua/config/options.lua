a79a41bd10821137c19ee3f3afa66907fa81db1b
ace <vuong753333@gmail.com>
2024-11-27 09:31:17 +0700

    fix: blink no longer to update to date the newest version

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -7,7 +7,6 @@ vim.g.loaded_perl_provider = 0
 vim.g.loaded_ruby_provider = 0
 vim.g.loaded_python3_provider = 0
 vim.g.loaded_node_provider = 0
-vim.g.lazyvim_blink_main = true
 vim.g.lazyvim_php_lsp = "intelephense"
 local opt = vim.opt
 opt.wildmode = "full"
