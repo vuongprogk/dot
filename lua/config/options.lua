1733c4c77445d40140582a3cb336822d65440f5d
ace <vuong753333@gmail.com>
2024-12-16 19:15:26 +0700

    feat: update options

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,14 +1,35 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
+
+if vim.g.neovide then
+  vim.o.guifont = "JetBrainsMono Nerd Font Mono:h18"
+  vim.g.neovide_scale_factor = 0.9
+  vim.g.neovide_detach_on_quit = "always_quit"
+end
 vim.g.loaded_perl_provider = 0
 vim.g.loaded_ruby_provider = 0
 vim.g.loaded_python3_provider = 0
 vim.g.loaded_node_provider = 0
+
 vim.g.lazyvim_php_lsp = "intelephense"
 local opt = vim.opt
 opt.wildmode = "full"
 vim.g.autoformat = false
 vim.opt.mousescroll = "ver:1,hor:6"
+
+-- better coop with fzf-lua
+vim.env.FZF_DEFAULT_OPTS = ""
+vim.g.ai_cmp = false
+vim.g.lazyvim_blink_main = not jit.os:find("Windows")
+
+vim.opt.backupdir = vim.fn.stdpath("state") .. "/backup"
+
+-- make all keymaps silent by default
+local keymap_set = vim.keymap.set
+---@diagnostic disable-next-line: duplicate-set-field
+vim.keymap.set = function(mode, lhs, rhs, opts)
+  opts = opts or {}
+  opts.silent = opts.silent ~= false
+  return keymap_set(mode, lhs, rhs, opts)
+end
