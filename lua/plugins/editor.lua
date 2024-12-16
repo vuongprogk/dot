81d7521f177ff86adf6fc4c5abd6eea7e196ad6a
ace <vuong753333@gmail.com>
2024-12-16 19:16:05 +0700

    feat: update editor config disable flash

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -1,14 +1,20 @@
 return {
-	"nvim-neo-tree/neo-tree.nvim",
-	opts = function(_, opts)
-		opts.event_handlers = opts.event_handlers and opts.event_handlers or {}
-		vim.list_extend(opts.event_handlers, {
-			{
-				event = "file_open_requested",
-				handler = function()
-					require("neo-tree.command").execute({ action = "close" })
-				end,
-			},
-		})
-	end,
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = function(_, opts)
+      opts.event_handlers = opts.event_handlers and opts.event_handlers or {}
+      vim.list_extend(opts.event_handlers, {
+        {
+          event = "file_open_requested",
+          handler = function()
+            require("neo-tree.command").execute({ action = "close" })
+          end,
+        },
+      })
+    end,
+  },
+  {
+    enabled = false,
+    "folke/flash.nvim",
+  },
 }
